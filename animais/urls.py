b5803b1aa41464b698fdc89b575dc9a3413064c4
Williames Camilo <williamescw@gmail.com>
2024-09-19 02:34:15 -0300

    Delete Button/Function Implementation

--- animais/urls.py
+++ animais/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
+from .views import (adicionar_diagnostico, editar_diagnostico,
+                    excluir_diagnostico, listar_diagnosticos)
 
 urlpatterns = [
     path('', views.listar_diagnosticos, name='listar_diagnosticos'),
     path('adicionar/', views.adicionar_diagnostico, name='adicionar_diagnostico'),
     path('diagnostico/editar/<int:pk>/', views.editar_diagnostico, name='editar_diagnostico'),
+    path('excluir/<int:pk>/', excluir_diagnostico, name='excluir_diagnostico'),
 ]
--- animais/views.py
+++ animais/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import DiagnosticoGestacaoForm
@@ -31,3 +32,30 @@ def editar_diagnostico(request, pk):
     
     # Certifique-se de que o template está na pasta correta
     return render(request, 'animais/editar_diagnostico.html', {'form': form})
+
+#Busca
+def listar_diagnosticos(request):
+    query = request.GET.get('q')
+    if query:
+        # Filtra os diagnósticos de acordo com o campo de pesquisa
+        diagnosticos = DiagnosticoGestacao.objects.filter(
+            Q(brinco__icontains=query) |
+            Q(reprodutor__icontains=query) |
+            Q(resultado__icontains=query)
+        )
+    else:
+        # Caso não haja pesquisa, mostra todos os diagnósticos
+        diagnosticos = DiagnosticoGestacao.objects.all()
+    
+    return render(request, 'animais/listar_diagnosticos.html', {'diagnosticos': diagnosticos})
+
+
+
+def excluir_diagnostico(request, pk):
+    diagnostico = get_object_or_404(DiagnosticoGestacao, pk=pk)
+    if request.method == 'POST':
+        diagnostico.delete()
+        return redirect('listar_diagnosticos')
+    else:
+        # Normalmente não chegaria aqui, pois é uma exclusão via POST, mas podemos redirecionar para a lista de diagnósticos em caso de acesso indevido.
+        return redirect('listar_diagnosticos')
